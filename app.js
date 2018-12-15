aaff536d1404363637854b26d5a48d2644e13c67
jgpro <kingjgpro@gmail.com>
2018-12-15 13:30:03 +1300

    Update app.js

--- app.js
+++ app.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
   res.write(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
   res.end();
 }).listen(8080);
+console.log(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
