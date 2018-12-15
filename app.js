21d05f148756b405cf53e4c78ea68a0f1a223c27
jgpro <kingjgpro@gmail.com>
2018-12-15 13:26:28 +1300

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,6 @@
 const { DateTime } = require("luxon");
-console.log(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
+const http = require("http");
+http.createServer(function (req, res) {
+  res.write(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
+  res.end();
+}).listen(8080);
