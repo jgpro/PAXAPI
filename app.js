028209541706e2c117ab9fc3ffb89f2c2916d230
jgpro <kingjgpro@gmail.com>
2018-12-15 14:34:51 +1300

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 var data = DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds();
 http.createServer(function (req, res) {
   res.write(`<p><a href="#">Hello</a> World</p>`);
-  console.log(data);
+  console.log(toString(data));
   res.end();
 }).listen(process.env.PORT);
 console.log(data);
