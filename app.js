d53cbf0812f39dbbebd877b3c9c9474cdabc7dc0
jgpro <kingjgpro@gmail.com>
2018-12-15 14:12:04 +1300

    Update app.js

--- app.js
+++ app.js
@@ -3,5 +3,5 @@ const http = require("http");
 http.createServer(function (req, res) {
   res.write(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
   res.end();
-}).listen(8080);
+}).listen(process.env.PORT);
 console.log(DateTime.utc().startOf('month').plus( { months: 1 } ).toSeconds());
